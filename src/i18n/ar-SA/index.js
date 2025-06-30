eb50263e3bff5147d2c3a4f9a09e9455557aa395
Logshoy <logshoy@gmail.com>
2025-06-30 10:13:44 +0500

    add ar-SA

---
+++ src/i18n/ar-SA/index.js
@@ -0,0 +1,17 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  failed: 'Action failed',
+  success: 'Action was successful123123',
+  language: 'Lang',
+  configuration: 'Сonfiguration',
+  directories: 'Directories',
+  shops: 'Shops',
+  goods: 'Goods',
+  cashboxs: 'Cashboxs',
+  services: 'Services',
+  exit: 'Exit',
+  devices: 'Devices',
+  users: 'Users',
+}
