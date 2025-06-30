9dac2dccf780ff33984f22f8b18303a21cec8098
Logshoy <logshoy@gmail.com>
2025-06-30 10:15:56 +0500

    add kk-KZ

---
+++ src/i18n/kk-KZ/index.js
@@ -0,0 +1,17 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  failed: 'Неудача',
+  success: 'Успешно',
+  language: 'Язык',
+  configuration: 'Конфигурация',
+  directories: 'Справочники',
+  shops: 'Магазины',
+  goods: 'Товары',
+  cashboxs: 'Кассы',
+  services: 'Сервисы',
+  exit: 'Выход',
+  devices: 'Устройства',
+  users: 'Пользователи',
+}
